8d0eceea28a29551a74d4dce41ae6f1df86fafd9
teon <subteon@gmail.com>
2013-04-11 07:40:13 +0900

    var and if sdded.

--- servers_check/servers_check.sh
+++ servers_check/servers_check.sh
@@ -5,6 +5,10 @@
 # servers_check.sh
 # auther : teon
 
+# with files
+# -- servers_list.txt
+# -- servers_check.log
+
 #-------
 # $ sh servers_check.sh servers_list.txt
 #-------
@@ -20,8 +24,15 @@
 
 for i in $(cat $1)
 do
-  echo $i
-  wget -nv --spider --timeout 60 $i 2>&1 | grep -c '200 OK'
+  date_str=`date +%F_%T`
+  status=`wget -nv --spider --timeout 60 $i 2>&1 | grep -c '200 OK'`
+
+  if [ $status -eq 0 ]
+  then
+    status_str="alert"
+  fi
+
+  echo $date_str $i $status $status_str >> server_check.log
 done
 
 exit 0
