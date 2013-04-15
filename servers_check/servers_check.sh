89eeb9464fe57f33c98b9e474f4e30005ce44810
teon <subteon@gmail.com>
2013-04-15 20:38:43 +0900

    cron mode->fixed.

--- servers_check/servers_check.sh
+++ servers_check/servers_check.sh
@@ -20,6 +20,8 @@
 
 #text = cat `./servers_list.txt`
 
+alert_log="./server_alert.log"
+
 [ -f $1 ] || exit 1
 
 for i in $(cat $1)
@@ -30,9 +32,13 @@ do
   if [ $status -eq 0 ]
   then
     status_str="alert"
+    echo $date_str $i $status $status_str >> $alert_log
   fi
 
   echo $date_str $i $status $status_str >> server_check.log
+  
+  sleep 1s
+  
 done
 
 exit 0
