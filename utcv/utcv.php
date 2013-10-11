e01f4b625e78d47a3760c6b552f9a30f43ea33e4
teon <subteon@gmail.com>
2013-10-11 21:38:51 +0900

    utcv file added. -> emg7

---
+++ utcv/utcv.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+// unixtime converter.
+// - on php.
+// -- for use terminal.
+// auther: teon
+*/
+
+if($argc){
+        if(argv[1]){
+		//params
+		
+	}else{
+		//no params
+		
+	}
+}else{
+
+	
+
+}
+
+
+function checkDate($datetime){
+
+	if (!preg_match('/^(\d\d)\/(\d\d)\/(\d\d\d\d) (\d\d):(\d\d)$/', $datetime, $m)) {
+	}
+
+}
+
+?>
